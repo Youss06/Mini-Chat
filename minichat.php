5cfbb2045a7b2e45ba0e10d95023f0daf979023e
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-13 15:18:49 +0200

    Add deskopt

---
+++ minichat.php
@@ -0,0 +1,5 @@
+<form class="" action="index.html" method="post">
+<p>Pseudo</p><input type="text" name="" value=""><br />
+<p>Message</p><input type="text" name="" value=""><br />
+<button type="submit" name="button">Envoyer</button>
+</form>
