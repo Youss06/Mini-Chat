df8f077fb3ab2bda1cc6a2578f05baf4487b5ab9
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-14 11:02:08 +0200

    Add link to slq

--- minichat_post.php
+++ minichat_post.php
@@ -9,5 +9,14 @@ die('Erreur : ' . $e->getMessage());
 
 }
 
+$req = $bdd->prepare('INSERT INTO messages(pseudo, message)
+ VALUES(:pseudo, :message)');
+
+$req->execute(array(
+    'pseudo' => $_POST['pseudo'],
+    'message' => $_POST['message']
+    ));
+
+header('Location: minichat.php');
 
  ?>
