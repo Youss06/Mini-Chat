a2cabebe8e94ba31fcf01e37f0096805d43e8602
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-14 10:18:35 +0200

    Add php

--- minichat.php
+++ minichat.php
@@ -1,5 +1,42 @@
-<form class="" action="index.html" method="post">
-<p>Pseudo</p><input type="text" name="" value=""><br />
-<p>Message</p><input type="text" name="" value=""><br />
-<button type="submit" name="button">Envoyer</button>
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mini-Chat</title>
+  </head>
+  <body>
+    <form class="" action="minichat_post.php" method="post">
+    <label for="pseudo">Pseudo :</label> <input type="text" name="" value=""><br />
+    <label for="message">Message :</label> <input type="text" name="" value=""><br />
+    <button type="submit" name="button">Envoyer</button>
+    </form>
+
+
+    <?php
+
+    try {
+
+      $bdd = new PDO('mysql:host=localhost;dbname=Mini-Chat;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+    } catch (Exception $e) {
+
+
+    die('Erreur : ' . $e->getMessage());
+
+
+    }
+
+$reponse = $bdd->query('SELECT * FROM messages');
+
+while ($donnees = $reponse->fetch())
+{
+  echo $donnees['pseudo'] .'<br />' ;
+}
+
+
+$reponse->closeCursor();
+
+     ?>
+
+  </body>
+</html>
